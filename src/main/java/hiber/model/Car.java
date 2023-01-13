3534c57ecd477d1bab5829b5eb7338a7e4a35191
alexandr_snurnitsyn <alexsnurnitsyn@mail.ru>
2023-01-13 14:06:39 +0600

    fix code

--- src/main/java/hiber/model/Car.java
+++ src/main/java/hiber/model/Car.java
@@ -16,9 +16,6 @@ public class Car {
     @Column(name = "series")
     private int series;
 
-    @OneToOne(mappedBy = "userCar")
-    private User user;
-
     public Car(){
     }
 
@@ -51,21 +48,12 @@ public class Car {
         this.series = series;
     }
 
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
     @Override
     public String toString() {
         return "Car{" +
                 "id=" + id +
                 ", model='" + model + '\'' +
                 ", series=" + series +
-                ", user=" + user +
                 '}';
     }
 }
